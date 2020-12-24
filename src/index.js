877e805ca8fe82b1099ff15f94d51f0f6f3bb625
Mrudang Shah <mrudang.shah@iflair.com>
2020-12-24 17:41:51 +0530

    server configuration

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-// Deep diving into GraphQL
+import express from "express";
+
+const app = express()
+
+app.get("/", (req,res) => {
+  res.send("Server is Up and Running !!!")
+})
+
+app.listen(6500, () => console.log("server is running at 6500 port"))
