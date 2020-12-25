4d5c6bb49dd58d266287c5c521894e9e621e2739
Mrudang Shah <mrudang.shah@iflair.com>
2020-12-25 12:04:27 +0530

    api built up

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import express from "express";
+import resolvers from "./resolvers";
+import schema from "./schema";
+
+import { graphqlHTTP } from "express-graphql"
 
 const app = express()
 
 app.get("/", (req,res) => {
   res.send("Server is Up and Running !!!")
 })
+
+const root = resolvers
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}))
+
 
 app.listen(6500, () => console.log("server is running at 6500 port"))
